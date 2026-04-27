36b9702e36eaf249ccb4e2fc2acb4de0a75de951
cristian <129994548+icristianhernandez@users.noreply.github.com>
2026-04-27 19:03:57 -0400

    feat(db): all Venezuelan states + major cities seeded

--- supabase/migrations/20260303021549_seed-geographical.sql
+++ supabase/migrations/20260303021549_seed-geographical.sql
@@ -17,19 +17,113 @@ on conflict (country_name) do nothing;
 
 insert into states (country_id, state_name)
 select
-    countries.id as country_id,
-    'Distrito Capital' as state_name
-from countries
-where countries.country_name = 'Venezuela'
+    c.id as country_id,
+    s.state_name
+from (
+    values
+    ('Amazonas'),
+    ('Anzoategui'),
+    ('Apure'),
+    ('Aragua'),
+    ('Barinas'),
+    ('Bolivar'),
+    ('Carabobo'),
+    ('Cojedes'),
+    ('Delta Amacuro'),
+    ('Distrito Capital'),
+    ('Falcon'),
+    ('Guarico'),
+    ('La Guaira'),
+    ('Lara'),
+    ('Merida'),
+    ('Miranda'),
+    ('Monagas'),
+    ('Nueva Esparta'),
+    ('Portuguesa'),
+    ('Sucre'),
+    ('Tachira'),
+    ('Trujillo'),
+    ('Yaracuy'),
+    ('Zulia')
+) as s (state_name)
+cross join countries as c
+where c.country_name = 'Venezuela'
 on conflict (state_name) do update
     set country_id = excluded.country_id;
 
 insert into cities (state_id, city_name)
 select
-    states.id as state_id,
-    'Caracas' as city_name
-from states
-where states.state_name = 'Distrito Capital'
+    st.id as state_id,
+    c.city_name
+from (
+    values
+    ('Amazonas', 'Puerto Ayacucho'),
+    ('Anzoategui', 'Barcelona'),
+    ('Anzoategui', 'Puerto La Cruz'),
+    ('Anzoategui', 'El Tigre'),
+    ('Anzoategui', 'Anaco'),
+    ('Anzoategui', 'Lecheria'),
+    ('Apure', 'San Fernando de Apure'),
+    ('Aragua', 'Maracay'),
+    ('Aragua', 'Turmero'),
+    ('Aragua', 'Cagua'),
+    ('Aragua', 'La Victoria'),
+    ('Aragua', 'Palo Negro'),
+    ('Aragua', 'El Limon'),
+    ('Barinas', 'Barinas'),
+    ('Bolivar', 'Ciudad Bolivar'),
+    ('Bolivar', 'Ciudad Guayana'),
+    ('Bolivar', 'Upata'),
+    ('Carabobo', 'Valencia'),
+    ('Carabobo', 'Puerto Cabello'),
+    ('Carabobo', 'Naguanagua'),
+    ('Carabobo', 'San Diego'),
+    ('Carabobo', 'Guacara'),
+    ('Carabobo', 'Los Guayos'),
+    ('Cojedes', 'San Carlos'),
+    ('Delta Amacuro', 'Tucupita'),
+    ('Distrito Capital', 'Caracas'),
+    ('Falcon', 'Coro'),
+    ('Falcon', 'Punto Fijo'),
+    ('Guarico', 'San Juan de los Morros'),
+    ('Guarico', 'Calabozo'),
+    ('Guarico', 'Valle de la Pascua'),
+    ('Guarico', 'Zaraza'),
+    ('La Guaira', 'La Guaira'),
+    ('Lara', 'Barquisimeto'),
+    ('Lara', 'Cabudare'),
+    ('Lara', 'Carora'),
+    ('Merida', 'Merida'),
+    ('Merida', 'El Vigia'),
+    ('Merida', 'Ejido'),
+    ('Miranda', 'Los Teques'),
+    ('Miranda', 'Petare'),
+    ('Miranda', 'Charallave'),
+    ('Miranda', 'Guarenas'),
+    ('Miranda', 'Guatire'),
+    ('Miranda', 'Cua'),
+    ('Miranda', 'Ocumare del Tuy'),
+    ('Miranda', 'Santa Teresa del Tuy'),
+    ('Miranda', 'Carrizal'),
+    ('Monagas', 'Maturin'),
+    ('Nueva Esparta', 'La Asuncion'),
+    ('Nueva Esparta', 'Porlamar'),
+    ('Nueva Esparta', 'Pampatar'),
+    ('Portuguesa', 'Guanare'),
+    ('Portuguesa', 'Acarigua'),
+    ('Portuguesa', 'Araure'),
+    ('Sucre', 'Cumana'),
+    ('Sucre', 'Carupano'),
+    ('Tachira', 'San Cristobal'),
+    ('Trujillo', 'Trujillo'),
+    ('Trujillo', 'Valera'),
+    ('Yaracuy', 'San Felipe'),
+    ('Zulia', 'Maracaibo'),
+    ('Zulia', 'Cabimas'),
+    ('Zulia', 'Ciudad Ojeda'),
+    ('Zulia', 'Machiques')
+) as c (state_name, city_name)
+inner join states as st on c.state_name = st.state_name
 on conflict (city_name) do update
     set state_id = excluded.state_id;
 
